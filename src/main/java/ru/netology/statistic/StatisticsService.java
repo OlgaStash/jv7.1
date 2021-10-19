cae4123365be1b627f5addf00fd584d2fa6021fd
Olga Stashenyuk <olgastachenyk@mail.ru>
2021-10-19 18:51:55 +0300

    Changed tests

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -15,5 +15,47 @@ public class StatisticsService {
                 current_max = income;
         return current_max;
     }
+
+    public long findMin(long[] incomes) {
+        long current_min_index = 0;
+        long current_min = incomes[0];
+        for (long income : incomes)
+            if (current_min > income)
+                current_min = income;
+        return current_min;
+    }
+
+    public long findAver(long[] incomes) {
+        long average_index = 0;
+        for (long income : incomes)
+            average_index = income + average_index;
+        return ((average_index) / incomes.length);
+    }
+
+    public long findSum(long[] incomes) {
+        long sum = 0;
+        for (long income : incomes)
+            sum += income;
+        return sum;
+    }
+
+    public int countAboveAver(long[] incomes) {
+        int count = 0;
+        for (long income : incomes) {
+            if (income > findAver(incomes)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public int countBelowAver(long[] incomes) {
+        int count = 0;
+        for (long income : incomes) {
+            if (income < findAver(incomes)) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
 
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,5 +18,66 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMin() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 3;
+
+        long actual = service.findMin(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findAver() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 7;
+
+        long actual = service.findAver(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findSum() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 85;
+
+        long actual = service.findSum(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldFindCountAboveAver() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        int expected = 6;
+
+        int actual = service.countAboveAver(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindCountBelowAver() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        int expected = 5;
+
+        int actual = service.countBelowAver(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+
+
 }
 
