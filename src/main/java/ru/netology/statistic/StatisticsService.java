686f341fe482223a3a44f3a635bd669b4dd494df
Olga Stashenyuk <olgastachenyk@mail.ru>
2021-10-21 16:16:42 +0300

    Added plagins Spotbug:check, CheckStyle:check

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -3,19 +3,20 @@ package ru.netology.statistic;
 public class StatisticsService {
 
     public long findMax(long[] incomes) {
-        long current_max_index = 0;
         long current_max = incomes[0];
-        for (long income : incomes)
-            if (income > current_max);
+        for (long income : incomes) {
+            if (income >= current_max)
+                current_max = income;
+        }
         return current_max;
     }
 
     public long findMin(long[] incomes) {
-        long current_min_index = 0;
         long current_min = incomes[0];
-        for (long income : incomes)
-            if (current_min > income)
+        for (long income : incomes) {
+            if (current_min >= income)
                 current_min = income;
+        }
         return current_min;
     }
 
