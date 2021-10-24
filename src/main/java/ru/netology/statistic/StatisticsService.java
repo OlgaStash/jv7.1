a883aa9fb1f70727a228b9215a31fde98ca00387
Olga Stashenyuk <olgastachenyk@mail.ru>
2021-10-24 20:51:33 +0300

    one method

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -11,47 +11,5 @@ public class StatisticsService {
         return current_max;
     }
 
-    public long findMin(long[] incomes) {
-        long current_min = incomes[0];
-        for (long income : incomes) {
-            if (current_min >= income)
-                current_min = income;
-        }
-        return current_min;
-    }
-
-    public long findAver(long[] incomes) {
-        long average_index = 0;
-        for (long income : incomes)
-            average_index = income + average_index;
-        return ((average_index) / incomes.length);
-    }
-
-    public long findSum(long[] incomes) {
-        long sum = 0;
-        for (long income : incomes)
-            sum += income;
-        return sum;
-    }
-
-    public int countAboveAver(long[] incomes) {
-        int count = 0;
-        for (long income : incomes) {
-            if (income > findAver(incomes)) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public int countBelowAver(long[] incomes) {
-        int count = 0;
-        for (long income : incomes) {
-            if (income < findAver(incomes)) {
-                count++;
-            }
-        }
-        return count;
-    }
 }
 
