341f9e8e2c8e993a20ec2ba6b2e336b8b7394a6e
Olga Stashenyuk <olgastachenyk@mail.ru>
2021-10-26 18:26:19 +0300

    Added test shouldFindMaxTest

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,12 +7,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
+
+
     @Test
-    void findMax() {
+    void shouldFindMax() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldFindMaxTest() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {11, 15, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 15;
 
         long actual = service.findMax(incomesInBillions);
 
